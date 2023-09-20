4259ec7bd16aa245a56c49079221935d041e7793
megahomyak <g.megahomyak@gmail.com>
2023-09-20 21:12:16 +0500

    Decided to make it much simpler *for now*

--- m_json_db/__init__.py
+++ m_json_db/__init__.py
@@ -1,37 +1,13 @@
 import json
-import threading
-
-class Accessor:
-
-    def __init__(self, value):
-        self.value = value
-        self.was_mutated = False
-
-    def __getitem__(self, key):
-        return self.value[key]
-
-    def __setitem__(self, key, value):
-        self.was_mutated = True
-        self.value[key] = value
-
-_thread_local_storages = threading.local()
-
-def _get_accessor_stack():
-    if not hasattr(_thread_local_storages, "accessor_stack"):
-        _thread_local_storages.accessor_stack = []
-    return _thread_local_storages.accessor_stack
 
 class Db:
     def __init__(self, path, *, default):
         self.path = path
         self.value = default
-        self.taker_thread_id = None
         self.reload()
 
     def __enter__(self):
-        accessor = Accessor(self.value)
-        _get_accessor_stack().append(accessor)
-        return accessor
+        pass
 
     def save(self):
         with open(self.path, "w", encoding="utf-8") as f:
@@ -45,6 +21,10 @@ class Db:
             pass
 
     def __exit__(self):
-        accessor = _get_accessor_stack().pop()
-        if accessor.was_mutated:
-            self.save()
+        self.save()
+
+    def __getitem__(self, key):
+        return self.value[key]
+
+    def __setitem__(self, key, value):
+        self.value[key] = value
