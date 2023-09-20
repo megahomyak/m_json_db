b624a160c2ebd829155434bb28998fd9c570c627
megahomyak <g.megahomyak@gmail.com>
2023-09-20 21:08:02 +0500

    Tried to make it thread-safe

--- m_json_db/__init__.py
+++ m_json_db/__init__.py
@@ -1,22 +1,50 @@
 import json
+import threading
+
+class Accessor:
+
+    def __init__(self, value):
+        self.value = value
+        self.was_mutated = False
+
+    def __getitem__(self, key):
+        return self.value[key]
+
+    def __setitem__(self, key, value):
+        self.was_mutated = True
+        self.value[key] = value
+
+_thread_local_storages = threading.local()
+
+def _get_accessor_stack():
+    if not hasattr(_thread_local_storages, "accessor_stack"):
+        _thread_local_storages.accessor_stack = []
+    return _thread_local_storages.accessor_stack
 
 class Db:
     def __init__(self, path, *, default):
         self.path = path
-        try:
-            with open(path, encoding="utf-8") as f:
-                self.value = json.loads(f.read())
-        except FileNotFoundError:
-            self.value = default
+        self.value = default
+        self.taker_thread_id = None
+        self.reload()
 
     def __enter__(self):
-        pass
+        accessor = Accessor(self.value)
+        _get_accessor_stack().append(accessor)
+        return accessor
 
-    def __exit__(self, *args):
+    def save(self):
         with open(self.path, "w", encoding="utf-8") as f:
             f.write(json.dumps(self.value, ensure_ascii=False))
 
-    def __getattr__(self, attrname):
-        return getattr(self.value, attrname)
+    def reload(self):
+        try:
+            with open(self.path, encoding="utf-8") as f:
+                self.value = json.loads(f.read())
+        except FileNotFoundError:
+            pass
 
-__all__ = ["Db"]
+    def __exit__(self):
+        accessor = _get_accessor_stack().pop()
+        if accessor.was_mutated:
+            self.save()
